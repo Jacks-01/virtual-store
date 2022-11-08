157089ecaeae2928d2dd011afc457c3271e82efe
Jack Stubblefield <Jack011601@gmail.com>
2022-11-08 11:58:18 -0600

    created category component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Products from "./Components/Products";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "./theme";
 import { CssBaseline } from "@mui/material";
+import Category from "./Components/Category";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <Header />
+      <Category/>
       <Products />
     </ThemeProvider>
 
---
+++ src/Components/Category/index.jsx
@@ -0,0 +1,12 @@
+import { Container, Typography } from "@mui/material";
+
+const Category = () => {
+    return (
+        <Container sx={{mt: '200px'}}>
+            <Typography> CATEGORY </Typography>
+            <Typography> Description goes here ...</Typography>
+        </Container>
+     );
+}
+ 
+export default Category;
